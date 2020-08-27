693c40866a375db9d33ef077f3a7f5b5bf50e009
zach1423 <70348930+zach1423@users.noreply.github.com>
2020-08-27 18:31:35 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -6,6 +6,26 @@ class gui
 {
   public static void main(String args[])
   {
+    //Creating the Frame
+    JFrame frame = new JFrame("Java to Python trranslator");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300,300);
     
+    //Creating the MenuBar and adding components
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menu1 = new JMenu("FILE");
+    JMenu menu2 = new JMenu("Help");
+    menuBar.add(menu1);
+    menuBar.add(menu2);
+    JMenuItem m11 = new JMenuItem("Open");
+    JMenuItem m22 = new JMenuItem("Save as");
+    menu1.add(m11);
+    menu1.add(m22);
+    
+    JButton button = new JButton("Press");
+    
+    frame.getContentPane().add(button); // Adds Button to content pane of frame
+    
+    frame.setVisible(true);
   }
 }
