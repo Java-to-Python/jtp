2e944fd409a889169107a2b03ff48bb4aa93f270
zach1423 <burtonzachery61@gmail.com>
2020-10-18 22:29:48 -0400

    Recursive

--- Java_Parser.java
+++ Java_Parser.java
@@ -1,28 +1,13 @@
 // This file will be for the reader.
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class read
-{
-  public static void file_reader(string file)
-  {
-    BufferedReader reader;
-    try {
-      reader = new BufferedReader( new FileReader( file));
-      String line = reader.readLiine();
-      while (line != null)
-      {
-        //translates code and writes code to file
-        
-        line = reader.readLine();
-        
-      }
-      reader.close();
+public class Java_Parser {
+
+    public static void parser(String file) {
+
     }
-    catch (IOExcpetiion e)
-    {
-      e.printStackTrace();
-    }
-  }
 }
+//
--- Writing_class.java
+++ Writing_class.java
@@ -1,8 +1,9 @@
 // This file is for the writer that will write the code to a file
-Public class writerSystem
-{
-  public static void writer()
-  {
-  
-  }
+
+public class Writing_class {
+
+    public static String writer(String input) {
+        String output = Java_To_Python_Translator.translate(input);
+        return output;
+    }
 }
