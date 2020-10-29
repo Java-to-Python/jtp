a3ff3a9253a22cdbbd41de70ce0a17655a812da5
Naquan Smith <45046512+ncsQuan@users.noreply.github.com>
2020-10-29 17:13:42 -0400

    Revert

---
+++ Java_To_Python_Translator.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Java_To_Python_Translator
+{
+  public static String translate(String input)
+  {
+            ArrayList<ArrayList<String>> translated = new ArrayList<>();
+            ArrayList<Token> tokens = new ArrayList<>();
+          
+	  Lexer lexer = new Lexer(input);
+	  String output ="";
+	  int index=0;
+	  
+		Token token = new Token(tokentype.Unknown, 0,"");
+		while(index!=input.lastIndexOf("}")) {	
+				token = lexer.nextToken();
+				
+				if(token.getType()==tokentype.End){
+					break;			
+				}
+				
+				
+				tokens.add(token);
+
+				
+				
+				System.out.println();
+				index++;
+		}
+                
+                translated = Interpreter.Translate(tokens);
+                
+     
+                for (int i = 0; i < translated.size(); i++) 
+                {
+                    for (int j = 0; j < translated.get(i).size(); j++) 
+                    {
+                        output += translated.get(i).get(j).toString();
+                    }
+
+                }
+		return output;
+  }
+}
+//
---
+++ keywords.java
@@ -0,0 +1,21 @@
+public enum keywords{ 
+                ABSTRACT, ASSERT, BOOLEAN, BREAK, BYTE, CASE,
+                CATCH, CHAR, CLASS, CONST, CONTINUE, DEFAULT,
+                DOUBLE, DO, ELSE, ENUM, EXTENDS, FALSE,
+                FINAL, FINALLY, FLOAT, FOR, GOTO, IF,
+                IMPLEMENTS, IMPORT, INSTANCEOF, INT, INTERFACE, LONG,
+                MAIN, NATIVE, NEW, NULL, PACKAGE,PRINTLN, PRIVATE, PROTECTED,
+                PUBLIC, RETURN, SHORT, STATIC, STRING, STRICTFP, SUPER,
+                SWITCH, SYNCHRONIZED, THIS, THROW, THROWS, TRANSIENT,
+                TRUE, TRY, VOID, VOLATILE, WHILE ;
+		
+		public static boolean compare(String c) {
+			keywords[] allk =keywords.values();
+			for(keywords k : allk) {
+				if(k.toString().equalsIgnoreCase(c)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
