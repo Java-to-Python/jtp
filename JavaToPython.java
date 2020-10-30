4f3f984f3fe94787472094d8fbee59801cb10ebf
Naquan Smith <45046512+ncsQuan@users.noreply.github.com>
2020-10-29 20:09:57 -0400

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit f01130670b50c461a40a814f1a0b6b2a8118c44f, reversing
    changes made to 29501a27e1380171750a8c727ff6644c3668759d.

--- JavaToPython.java
+++ JavaToPython.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
-public class JavaToPython {
+public class JavaToPython  
+{
+  public static void main(String[]args) {
+	  new gui();
+	
+  }
+ }
 
-    public static void main(String[] args) {
-        new gui();
-
-    }
-}
+//
